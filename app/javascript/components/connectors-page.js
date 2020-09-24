c9962660f3c19e35a518cd86e0d2474190ed9506
Lior Levy <lior.pmtraining@gmail.com>
2020-09-24 16:34:28 +0200

    reidrect uri

--- app/javascript/components/connectors-page.js
+++ app/javascript/components/connectors-page.js
@@ -1,7 +1,6 @@
 const toggles = document.querySelectorAll('.onoffswitch-checkbox');
 let url = ""
-if (window.location.href.includes('trackland')) {
-
+if (location.hostname.includes('local')) {
   url = `https://accounts.spotify.com/authorize?client_id=815d14d339184ba78c0d71ef9865863d&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fspotify%2Fcallback&response_type=code&scope=playlist-read-private+playlist-read-collaborative+playlist-modify-public+playlist-modify-private+user-library-modify+user-library-read+ugc-image-upload+user-read-private+user-read-email&show_dialog=false`
 } else {
   url = `https://accounts.spotify.com/authorize?client_id=815d14d339184ba78c0d71ef9865863d&redirect_uri=https%3A%2F%2Ftrackland.herokuapp.com%2Fauth%2Fspotify%2Fcallback&response_type=code&scope=playlist-read-private+playlist-read-collaborative+playlist-modify-public+playlist-modify-private+user-library-modify+user-library-read+ugc-image-upload+user-read-private+user-read-email&show_dialog=false`
